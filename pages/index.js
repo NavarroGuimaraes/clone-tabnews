538e10e2029ad9d56871eb7ebe71357951340a93
Navarro Guimarães. <navasxll@hotmail.com>
2025-10-22 02:37:16 +0000

    adicionando a frase correta

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Teste</h1>
+    return <h1>Amanda, Amor da minha vida, eu te amo demais!
+        Se tu também me ama demais, dê uma risadinha
+    </h1>
 }
 
 export default Home;
