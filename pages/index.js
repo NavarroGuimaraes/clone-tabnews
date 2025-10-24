1ef0424ea9941c2f525cb7f309bfc5e6c45319bf
Navarro Guimarães. <navasxll@hotmail.com>
2025-10-24 17:34:14 +0000

    adicionando scripts de `lint`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 function Home() {
-    return <h1>Amanda, amor da minha vida, eu te amo demais!
-        Se tu também me ama demais, dá uma risadinha! 🤭
+  return (
+    <h1>
+      Amanda, amor da minha vida, eu te amo demais! Se tu também me ama demais,
+      dá uma risadinha! 🤭
     </h1>
+  );
 }
 
 export default Home;
